20f0c10018b1e85cb90dd616882b5e0ec54ffd5a
soheib <bouafiasoheib918@gmail.com>
2026-02-20 17:55:09 +0100

    serializer done

--- project/tickets/models.py
+++ project/tickets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model) : 
+    hall = models.CharField(max_length=10)
+    Movie = models.CharField(max_length=10)
+    date = models.DateField()
+
+class Guest(models.Model) : 
+    name = models.CharField(max_length=30)
+    mobile = models.CharField(max_length=10)
+
+
+class Reservation(models.Model):
+    guest = models.ForeignKey(Guest, related_name='reservations', on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, related_name='reservations', on_delete=models.CASCADE)
