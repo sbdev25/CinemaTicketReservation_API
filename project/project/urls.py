fe8713dce3a536a15b185607d7dffb3dca5fbead
soheib <bouafiasoheib918@gmail.com>
2026-02-21 00:24:16 +0100

    url done

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tickets import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('no_module/' , views.no_rest_module)
 ]
--- project/tickets/views.py
+++ project/tickets/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http.response import JsonResponse
 # Create your views here.
+
+#1 without rest framework 
+
+def no_rest_module(requesr) : 
+    guests = [
+        {
+            'id' : 1 , 
+            'Name' : 'omar',
+            "mobile" : 789099,
+        },
+         {
+            'id' : 2 , 
+            'Name' : 'zino',
+            "mobile" : 569099,
+        }
+    ]
+    return JsonResponse(guests , safe=False)
